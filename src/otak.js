1a2b3b6ff466e0fe94df9b7bd6dbc96b9aa02ae4
Beimarsempai <87555802+Beimarsempai@users.noreply.github.com>
2021-07-17 23:19:10 -0400

    Add files via upload

---
+++ src/otak.js
@@ -0,0 +1,48 @@
+const otak = (prefix, pushname) => {
+    return `*Palabras especificas para que el bot interactue con ustedes mis queridos otakus*
+
+
+bañate
+pack
+gime
+quien es tu sempai?
+yamete
+buenas
+a
+sexual
+te amo
+konede
+kurumi
+bemos Netflix?
+Gimeme
+onichan
+senpai
+Ha!
+Goku
+Soy gracioso?
+Diego
+te importo?
+No sirves bot
+sate sate
+soy adicto?
+¡A
+Bot no sirves
+zdhun
+gay
+vete bot
+me gustan los trap
+roar
+un negro
+chistoso
+que?
+puedo jugar papi?
+pero
+Beimar
+
+_Ojito escribe tal como esta (mira bien)_
+
+ву Beimar`
+
+}
+
+exports.otak = otak
---
+++ src/shantera.js
@@ -0,0 +1,49 @@
+const shantera = (prefix, pushname) => {
+    return `*Palabras especificas para que el bot interactue con ustedes*
+
+
+    bañate
+    pack
+    gime
+    quien es tu sempai?
+    yamete
+    buenas
+    a
+    sexual
+    te amo
+    konede
+    kurumi
+    bemos Netflix?
+    Gimeme
+    onichan
+    senpai
+    Ha!
+    Goku
+    Soy gracioso?
+    Diego
+    te importo?
+    No sirves bot
+    sate sate
+    soy adicto?
+    ¡A
+    Bot no sirves
+    zdhun
+    gay
+    vete bot
+    me gustan los trap
+    roar
+    un negro
+    chistoso
+    que?
+    puedo jugar papi?
+    pero
+    Beimar
+
+
+_*Ojito escribe tal y como esta en el mensaje*_
+
+ву Beimar`
+
+}
+
+exports.shantera = shantera
